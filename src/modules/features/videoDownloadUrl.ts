9d98e2a55fe632f52d316815bda18a3d4b058a3a
reino08 <170168346+reino08@users.noreply.github.com>
2024-06-19 17:14:50 -0400

    add download buttons to video player context menu

---
+++ src/modules/features/videoDownloadUrl.ts
@@ -0,0 +1,46 @@
+import Logger from "../../logger.ts";
+import { patchHalves } from "../../patch.ts";
+import { React } from "../react.ts";
+import Webpack from "../webpack.ts";
+
+Webpack.getString("_handleCopyVideoAddress", x => x?.Z?.prototype?.render).then(exports => {
+    patchHalves(exports.Z.prototype, "render", undefined, (self, _, res) => {
+        if (!self.state.openContextMenu) return;
+        patchHalves(res.props, "children", undefined, (_, __, res) => {
+            let props = res.props.children[1].props;
+            let elements = [
+                React.createElement(props.children.type, {
+                    actionText: "Copy download link",
+                    onClick: () => call(navigator.clipboard.writeText.bind(navigator.clipboard), getVideoUrl(self)),
+                    withBottomBorder: false,
+                }),
+                React.createElement(props.children.type, {
+                    actionText: "Open video source",
+                    onClick: () => call(window.open, getVideoUrl(self)),
+                    withBottomBorder: false,
+                }),
+            ];
+            props.children = React.Children.toArray(props.children).concat(elements);
+        });
+    })
+});
+
+function call(func, val) {
+    if (val) func(val);
+}
+
+function getVideoUrl(self): string | void {
+    let videos = self.props.playerState.tracks
+        .flatMap(track => track.variants)
+        .filter(variant => variant.type == "video/mp4");
+
+    let best: any = undefined;
+    for (let video of videos) {
+        if (best?.bitrate || 0 < video.bitrate)
+            best = video;
+    }
+
+    if (!best)
+        return void Logger.warn("Failed to get download url for video:", self.props.playerState);
+    return best.src;
+}
