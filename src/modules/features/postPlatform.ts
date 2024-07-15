977c26b5abd12fd8289096f20d64df0322e53cc8
reino08 <170168346+reino08@users.noreply.github.com>
2024-07-14 23:05:13 -0400

    add null check to fix partial crash with post platform

--- src/modules/features/postPlatform.ts
+++ src/modules/features/postPlatform.ts
@@ -6,11 +6,13 @@ TweetUserPatch.then(patch => patch.subscribe(patch.post, (self, _, res) => {
     let tweet = findInTree(self._reactInternals, (x: any) => x.stateNode?.props?.tweet);
     if (!tweet) return res;
 
-    let platform = tweet.source_name
-        .replace(/Twitter for/, "")
-        .replace(/Twitter Web App/, "Web")
-        .replace(/Twitter Web Client/, "Web Client")
-        .replace(/TweetDeck Web App/, "TweetDeck")
+    let platform: string | undefined;
+    if (tweet.source_name)
+        platform = tweet.source_name
+            .replace(/Twitter for/, "")
+            .replace(/Twitter Web App/, "Web")
+            .replace(/Twitter Web Client/, "Web Client")
+            .replace(/TweetDeck Web App/, "TweetDeck")
 
     let props = res.props.children.props.children.props.children[1].props;
     props.children = React.Children.toArray(props.children).concat(
@@ -26,6 +28,6 @@ TweetUserPatch.then(patch => patch.subscribe(patch.post, (self, _, res) => {
                 minWidth: "content-fit",
                 flex: "1",
             },
-        }, platform || "Unknown", tweet.source_url)
+        }, platform || "Unknown", tweet.source_url || "/")
     );
 }));
