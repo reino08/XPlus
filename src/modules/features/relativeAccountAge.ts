bb1938f39aa52d78ee6ba316ae8aaef2d793fe40
reino08 <170168346+reino08@users.noreply.github.com>
2025-02-26 22:58:11 -0500

    add time to account join date

--- src/modules/features/relativeAccountAge.ts
+++ src/modules/features/relativeAccountAge.ts
@@ -6,6 +6,6 @@ window["dayjs"].extend(window["dayjs_plugin_relativeTime"]);
 extern_JoinDate.then(exports => {
     patchHalves(exports.Z, "type", undefined, (_, args, res) => {
         if (res?.props?.children)
-            res.props.children += ` (${window["dayjs"](args[0].joinDate).fromNow()})`;
+            res.props.children = `Joined ${new Date(args[0].joinDate).toLocaleString()} (${window["dayjs"](args[0].joinDate).fromNow()})`;
     })
 });
