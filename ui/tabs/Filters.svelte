5f745cc7f668c064e2907c71277d87b95e9a2b44
reino08 <170168346+reino08@users.noreply.github.com>
2025-04-27 23:04:15 -0400

    readd empty display tag and add visual distiction to headers

--- ui/tabs/Filters.svelte
+++ ui/tabs/Filters.svelte
@@ -10,50 +10,50 @@
 #
 # Example filters:
 
-# Empty Bio
-# b/^$/
+# -- Empty Bio
+# b/^$/ # Empty
 
-# War Spammers
+# -- War Spammers
 # /\ud83c\uddee\ud83c\uddf1/g # Israel Flag
 # /\ud83c\uddf5\ud83c\uddf8/g # Palestine Flag
 # /\ud83c\uddfa\ud83c\udde6/g # Ukraine Flag
 # /\ud83c\uddf7\ud83c\uddfa/g # Russian Flag
 # /\ud83c\udf49/g # Pro-Palestine Spammers
 
-# 18+/OF/NSFW accounts
+# -- 18+/OF/NSFW accounts
 # /\ud83d(\udd1e|\udc45)/g # NSFW account
 
-# Religious Spammers
+# -- Religious Spammers
 # /\u271d/g # Christians
 # /\u2721/g # Jews
 # /\u262a/g # Muslims
 # b/atheist/i # Atheists
 # b/agnostic/i # Agnostics
 
-# Roleplay/Alt Accounts
+# -- Roleplay/Alt Accounts
 # /(\s+)(uwu|owo|furry)/gi # RP
 # b/(nsfw|priv|private|main)\s*(:|acc|@)/gi # Alt
 # b/(ooc|parody)/gi # Parody
 
-# Groomers/Online Daters
+# -- Groomers/Online Daters
 # b/taken/gi # E-Dater
 # b/single/gi # E-Dater
 # b/tellonym.me\//g # E-Groomer
 
-# Minors
+# -- Minors
 # b/minor/gi # Minor
 # b/(?<![^\s])\s?1[0-7](?![^\s[:punct:]])/g # Minor
 
-# Annoying Poltical Spammers
+# -- Annoying Poltical Spammers
 # /\u262d/g # Communist Symbolism
 # b/liberal/gi # Liberals
 # b/conservative/gi # Conservatives
 
-# Sports/Celebrity Worshippers
+# -- Sports/Celebrity Worshippers
 # b/(\s+)stan(?<![a-z])/gi # Celebrity "stans"
 # b/(football|basketball|baseball|soccer|superbowl)/gi # Sports
 
-# Annoying Twitter Stuff
+# -- Annoying Twitter Stuff
 # /hrt/gim # Groomers
 # /(he|she|they|it|xe)\/(him|her|they|them|she|it|xer)/gi # Pronouns
 # b/(^|\s+)\/(j|hj|s|srs|nsrs|gen|tw)($|\s+)/g # Tone Indicators
