2229a3fed26ede03ca05f668f2c1eaeaaf295c29
reino08 <170168346+reino08@users.noreply.github.com>
2025-05-11 12:01:40 -0400

    save graphql to file

--- ui/tabs/Debug/GraphQL.svelte
+++ ui/tabs/Debug/GraphQL.svelte
@@ -4,12 +4,35 @@
   const data = channelOnce("debug.graphql.get", "debug.graphql.set").then(
     ([data]) => Object.entries(data)
   );
+
+  async function saveToFile() {
+    const element = document.createElement("a");
+    element.href = URL.createObjectURL(
+      new Blob(
+        [
+          (await data)
+            .map(
+              ([_, value]) =>
+                `${value.operationName}:${value.operationType}:${value.queryId}:${value.metadata.featureSwitches.join()}:${value.metadata.fieldToggles.join()}`
+            )
+            .join("\n"),
+        ],
+        { type: "text/plain" }
+      )
+    );
+    element.download = "graphql.txt";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+    URL.revokeObjectURL(element.href);
+  }
 </script>
 
 {#await data}
   <div>Debug data not found.</div>
 {:then data}
   <section>
+    <button on:click={saveToFile}>Save to file</button>
     {#each data as [key, value]}
       <details class="tab">
         <summary>
